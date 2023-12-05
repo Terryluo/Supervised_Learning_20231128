1d0fee07c6c1e00fcd9d38f78133d1689f3265d4
Zhiteng Luo(Terry) <terryluo256@gmail.com>
2023-12-04 21:59:16 -0500

    update Lab_04.py

--- Lab_04.py
+++ Lab_04.py
@@ -1,4 +1,4 @@
 import numpy as np
 import matplotlib.pyplot as plt
 plt.style.use('./deeplearning.mplstyle')
-from lab_utils_uni import plt_house_x, plt_contour_wgrad, plt_divergence
+from lab_utils_uni import plt_house_x, plt_contour_wgrad
